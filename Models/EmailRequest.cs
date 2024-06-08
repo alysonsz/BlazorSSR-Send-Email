c93ca19191ce0521d5be2f7caf5a9ba41034dc46
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2024-06-08 15:29:22 -0300

    blazor-send-email

---
+++ Models/EmailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorSSRSendEmail.Models
+{
+    public class EmailRequest
+    {
+        public string? To { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+    }
+}
